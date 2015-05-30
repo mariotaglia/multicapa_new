209dae7262c78abd1e8ab212359374bf11b97356
mario <mario@mate.bme.northwestern.edu>
2015-05-30 16:13:12 -0500

    in1n redefined as integer*1

--- modules.f90
+++ modules.f90
@@ -41,7 +41,7 @@ INTEGER maxcuantas
 
 integer curvature, radio
 
-integer*4, allocatable :: in1n(:,:,:,:)
+integer*1, allocatable :: in1n(:,:,:,:)
 integer*4, allocatable :: in1tmp(:)
 integer, allocatable ::  maxpos(:,:,:)
 integer, allocatable ::  minpos(:,:,:)
