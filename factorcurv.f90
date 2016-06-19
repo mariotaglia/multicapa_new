9c4310befb62c6e31306d7029a75dd1dc8b1a472
mario <mario@mate.bme.northwestern.edu>
2016-06-19 13:19:12 -0500

    fixed factor curv

--- factorcurv.f90
+++ factorcurv.f90
@@ -3,7 +3,16 @@ double precision function factorcurv (bas, pos)
 use multicapa
 implicit none
 integer bas, pos
+
+
+select case abs((curvature))
+case (0)
+factorcurv = 1.0
+case (1)
 factorcurv = (float(bas)-0.5)/(float(pos)-0.5)
+case (2)
+factorcurv = (float(bas)-0.5)/(float(pos)-0.5)**2
+end select
 
 return
 end
